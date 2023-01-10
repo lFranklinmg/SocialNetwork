ab3c38148ad4c792487f1bef68b46d66f292a2f8
Léo Monte <leomonte01@gmail.com>
2023-01-10 09:49:49 -0300

    firts commit

---
+++ API/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Removed by 
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Migrate automate and Seed Data
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    //await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError("An error ocurred during migration or Seed Service");
+}
+
+app.Run();
